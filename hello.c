a5c18deea94837ed2d3bfdd8d2f26845efbb8237
Gábor Hargitai <drummingdemon@users.noreply.github.com>
2015-08-17 19:42:47 +0200

    Created hello.c
    The infamous 'hello, world' text

---
+++ hello.c
@@ -0,0 +1,16 @@
+/*
+ *  hello.c
+ *
+ *  Harvard CS50x3 - Problem Set 1 - Hello
+ *
+ *  Prints the infamous 'hello, world' text to the standard output
+ *
+ *  Gábor Hargitai <gabriel.hargitai@gmail.com>
+ *
+ */
+ #include <stdio.h>
+
+int main(void)
+{
+    printf("hello, world\n");
+}
